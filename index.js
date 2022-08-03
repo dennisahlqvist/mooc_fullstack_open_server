2d7081a6fa228b574138f2cfd48a314d2d1787b8
dennisahlqvist <dennisahlqvist@hotmail.com>
2022-08-03 23:20:09 +0000

    Exercise 3.4

--- index.js
+++ index.js
@@ -47,6 +47,13 @@ app.get('/api/persons/:id', (request, response) => {
         response.status(404).end()
       }
   })
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+  
+    response.status(204).end()
+  })
   
   const PORT = 3001
   app.listen(PORT, () => {
